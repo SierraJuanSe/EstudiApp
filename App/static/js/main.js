cb67b8be3b12d94461160837c7056dfbfe7cbd5a
Johan Herrera <johan.herrera01@correo.usa.edu.co>
2020-06-09 22:41:40 -0500

    Agrego Funciones Al Login

--- App/static/js/main.js
+++ App/static/js/main.js
@@ -1,6 +1,42 @@
 var Ngrupos = 11;
 var aux = 12;
 
+
+var baseDeDatosUsuarios = [
+    {
+        correo: 'johan.herrera01@correo.usa.edu.co',
+        contraseña: '111',
+        nombre: 'Johan',
+        apellido: 'Herrera',
+        carrera: 'Ingenieria de Sistemas y Telecomunicaciones',
+        semestre: '(6)'
+    },
+    {
+        correo: 'juan.sierra01@correo.usa.edu.co',
+        contraseña: '222',
+        nombre: 'Juan',
+        apellido: 'Sierra',
+        carrera: 'Ingenieria de Sistemas y Telecomunicaciones',
+        semestre: '(6)'
+    },
+    {
+        correo: 'luis.velasquez01@correo.usa.edu.co',
+        contraseña: '333',
+        nombre: 'Luis',
+        apellido: 'Velasquez',
+        carrera: 'Ingenieria de Sistemas y Telecomunicaciones',
+        semestre: '(6)'
+    },
+    {
+        correo: 'oscarjulianreyest@gmail.com',
+        contraseña: '444',
+        nombre: 'Oscar',
+        apellido: 'Reyes',
+        carrera: 'Ingenieria de Sistemas y Telecomunicaciones',
+        semestre: '(6)'
+    }
+]
+
 $('#Cuenta').click(function () {
     $('#login').hide(1000);
     $('#estudiante').show(1000);
@@ -8,10 +44,23 @@ $('#Cuenta').click(function () {
 });
 
 $('#Ingresar').click(function () {
-    $('#fondo').hide(1000);
-    $('#menu').show(1000);
-    $('#menu1').show(1000);
-
+    var cumple=0;
+    var i=0;
+    var correoIni = $('#exampleInputEmail1').val();
+    var passIni = $('#exampleInputPassword1').val();
+    for (i=0; i <baseDeDatosUsuarios.length; i++) {
+        if(baseDeDatosUsuarios[i].correo==correoIni){
+            cumple=1;
+        }
+    }
+    if(cumple==1){
+        $('#fondo').hide(1000);
+        $('#menu').show(1000);
+        $('#menu1').show(1000);
+    }else{
+        swal("Error", "Verifica tus datos", "error")
+    }
+    
 });
 
 
@@ -67,15 +116,28 @@ $('#Crear').click(function () {
     var nom = $('#nombre').val();
     var ape = $('#apellido').val();
     var carre = $('#carrera').find('option:selected').text();
-    var semestre = $('#semestre').find('option:selected').text();
-    var correo = $('#correo').val();
+    var semestre1 = $('#semestre').find('option:selected').text();
+    var correo1 = $('#email').val();
     var pass = $('#password').val();
     var vpass = $('#verifypass').val();
 
+    var usuario ={
+        correo: correo1,
+        contraseña: pass,
+        nombre: nom,
+        apellido: ape,
+        carrera: carre,
+        semestre: semestre1
+    };
+
+
     if (pass == vpass) {
+
+        baseDeDatosUsuarios.push(usuario);
         swal("Muy bien!", "Has creado una cuenta!", "success").then((value => {
             $('#login').show(1000);
             $('#estudiante').hide(1000);
+            console.log(baseDeDatosUsuarios);
         }
 
         ));
